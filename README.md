2694ba218d663939fc217fc2389af463564b4fd1
hl662 <50554904+hl662@users.noreply.github.com>
2020-01-30 14:14:25 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Word Doc MLA Format Python Script
+
+---
+
+### Dependencies:
+
+    pip install python-docx
+
+## Description:
+
+I got tired of editing my font size, the line spacing and whatever I need else to write MLA formatted papers. Since sophomore year it seems like the amount of papers I've had to write has risen. So I automated the creation of a word document for myself. I mean, one purpose of programming is to make our lives easier right?
+
+Why not share it to others, save them some time. Also, why not demonstrate some of my knowledge in python?
+
+## Instructions:
+
+### Reminder:
+
+### Edit the script and change the name variable to your name!
+
+    if __name__ == "__main__":
+        name = "John Doe"
+
+The script will prompt you for your professor's name, your course name (Ex. CS-101-XXX) and the title of your paper. (I just copy the scripts and set the variables by myself for each course and professor)
+
+Feel free to omit any input if you don't remember haha, the main goal of this script is to create a word document with an MLA format ready. Plus, it opens the file for you if you want to get started immediately.
---
+++ docxMLAcreator.py
@@ -0,0 +1,57 @@
+from docx import Document
+from docx.shared import Pt, Inches
+from tkinter import Tk
+from tkinter import filedialog
+from datetime import date
+import os
+
+def setDirectory():
+    directory = filedialog.askdirectory()
+    root.destroy()
+    return directory 
+
+def createPath():
+    file_path = setDirectory()
+    file_name = input("Enter a name for your file:\n")
+    file_path += '/%s.docx' % (file_name)
+    return file_path
+
+def setMLAstyle(document):
+    style = document.styles['Normal']
+    paragraph_format = document.styles['Normal'].paragraph_format
+    paragraph_format.line_spacing = 1.5
+    font = style.font
+    font.name = 'Times New Roman'
+    font.size = Pt(12)
+    return
+
+def createFile(name,date,prof,course,title,file_path):
+    document = Document()
+    setMLAstyle(document)
+    sections = document.sections
+    for section in sections:
+        section.top_margin = Inches(1.0)
+        section.left_margin = Inches(1.0)
+        section.right_margin = Inches(1.0)
+
+    document.add_paragraph(name,style='Normal')
+    document.add_paragraph(prof,style='Normal')
+    document.add_paragraph(course,style='Normal')
+    document.add_paragraph(date,style='Normal')
+    titleP = document.add_paragraph(title,style='Normal')
+    titleP.paragraph_format.alignment = 1
+    document.save(file_path)
+    return 
+
+if __name__ == "__main__":
+    name = "John Doe"
+    root = Tk()
+    today = date.today()
+    date = today.strftime("%B %d, %Y")
+    file_path = createPath() 
+    
+    prof = 'Professor ' + input("Enter name of your professor:\n")
+    course = input("Enter course number and section number (Ex. CS-260-003, or however your school does it):\n")
+    title = input("Enter title of your paper:\n")
+    createFile(name,date,prof,course,title,file_path)
+    os.system('start %s' %(file_path))
